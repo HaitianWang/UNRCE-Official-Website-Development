66c2379766ad1259e79844fe682eccfc83df38cd
Ryan Takeda <ryantakeda1@gmail.com>
2023-08-21 15:51:01 +0800

    I don't Remember what I did

--- send-email.py
+++ send-email.py
@@ -3,14 +3,14 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 message = Mail(from_email="rce.uwa@gmail.com",
-               to_emails='ryantakeda1@gmail.com',
-               subject='First Test',
-               plain_text_content='This is my first sendgrid email, hopefully this works',
-               html_content='<strong>Testing html_content</strong>')
+               to_emails='tomr06072001@gmail.com',
+               subject='Password Reset Confirmation',
+               plain_text_content='This is my second sendgrid email',
+               html_content='<h1>Password Reset</h1><p> Please click <a href="www.youtube.com.au">here</a> to reset your password</p><i> Please Note: This link will only stay valid for 24 hours.</i>')
 
 try:
-    # sg = SendGridAPIClient('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')
-    sg = SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
+    sg = SendGridAPIClient('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')
+    # sg = SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
     response = sg.send(message)
     print(response.status_code)
     print(response.body)
