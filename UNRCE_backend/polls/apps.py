81423063629819d85a8a821c5d76ff7671183175
SimonDell <simonqiu4@gmail.com>
2023-08-22 17:23:33 +0800

    d

--- UNRCE_backend/polls/apps.py
+++ UNRCE_backend/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PoollsConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'poolls'
+    name = 'polls'
---
+++ UNRCE_backend/polls/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
