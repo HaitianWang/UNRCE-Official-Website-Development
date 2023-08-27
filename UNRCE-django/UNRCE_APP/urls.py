a5e61f6489665278626ca9adb6e5ad62db5df993
thomas rigby <tomr0607@hotmail.com>
2023-08-27 14:18:19 +0800

    WIP: ce9749a Merge pull request #2 from HaitianWang/simon

--- UNRCE-django/UNRCE_APP/urls.py
+++ UNRCE-django/UNRCE_APP/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import IndexView, SignUpView, UploadImageView
+from . import views
 
 app_name = "UNRCE_APP"
 
@@ -25,4 +26,7 @@ urlpatterns = [
       # Added upload view here, don't forget to
   # import UploadImageView from .views
   path("upload/", UploadImageView.as_view(), name="upload"),
+  path('forgot-password/', views.forgot_password, name='forgot-password'),
+  path('reset-password/', views.reset_password, name='reset-password'),
+
 ]
