a39a2bbee8365b5341201faeb7caee9d9a19871f
SimonDell <simonqiu4@gmail.com>
2023-08-31 23:38:29 +0800

    pm

--- UNRCE_backend/polls/views.py
+++ UNRCE_backend/polls/views.py
@@ -45,6 +45,15 @@ def vote(request, question_id):
 #     }
 #     return render(request, 'polls/index.html', context)
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
