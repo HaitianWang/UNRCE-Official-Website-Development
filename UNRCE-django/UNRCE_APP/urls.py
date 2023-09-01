8a68a2f435ba1bfc5db21f277a6400025bc0948a
SimonDell <simonqiu4@gmail.com>
2023-09-01 11:33:08 +0800

    ged

--- UNRCE-django/UNRCE_APP/urls.py
+++ UNRCE-django/UNRCE_APP/urls.py
@@ -1,28 +1,27 @@
 from django.urls import path, reverse_lazy
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import IndexView, SignUpView, UploadImageView
+# Import the built-in views from Django with alias
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
+from .views import ForgetPasswordView, ResetPasswordView, IndexView, SignUpView, UploadImageView
 
 app_name = "UNRCE_APP"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path(
-        "login/",
-        LoginView.as_view(
+    
+    path("login/", AuthLoginView.as_view(
             template_name="UNRCE_APP/login.html",
             success_url=reverse_lazy("UNRCE_APP:index"),
             redirect_authenticated_user=True,
-        ),
-        name="login",
-    ),
-    path(
-        "logout/",
-        LogoutView.as_view(next_page=reverse_lazy("UNRCE_APP:index")),
-        name="logout",
-    ),
-    # Added signup view here
+        ), name="login"),
+    
+    path("logout/", AuthLogoutView.as_view(
+            next_page=reverse_lazy("UNRCE_APP:index")
+        ), name="logout"),
+    
     path("signup/", SignUpView.as_view(), name="signup"),
-      # Added upload view here, don't forget to
-  # import UploadImageView from .views
-  path("upload/", UploadImageView.as_view(), name="upload"),
+    
+    path("upload/", UploadImageView.as_view(), name="upload"),
+    
+    path('forget_password/', ForgetPasswordView.as_view(), name='forget_password'),
+    path('reset_password/<uidb64>/<token>/', ResetPasswordView.as_view(), name='reset_password'),
 ]
