21e934578b78178dbd27cc37b8a2cee1d7ce4ae6
James Lee <jameslee6352@gmail.com>
2023-09-07 00:00:21 +0800

    Broad test structuring

---
+++ UNRCE-django/UNRCE_APP/tests/test_selenium.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+import UNRCE_APP
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+class TestLogin(TestCase):
+    pass 
+
+class TestSignUp(TestCase):
+    pass
+
+class TestProjectSubmission(TestCase):
+    pass
+
