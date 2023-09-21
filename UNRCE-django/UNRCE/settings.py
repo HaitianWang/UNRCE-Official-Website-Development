f9cd59efc7be6afeb3c0c9d93c2ddbcb33ba7e9c
James Lee <jameslee6352@gmail.com>
2023-09-21 14:31:37 +0800

    Allow local hosting

--- UNRCE-django/UNRCE/settings.py
+++ UNRCE-django/UNRCE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*4#w1xtd7#hfw_kyd$(4pwlv&@uv9xbzgxg8!e1d-g9dp!r@p7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.24.216.179']
+ALLOWED_HOSTS = ['3.24.216.179','.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
