04d82e7cc9794f429938a71af9ec9ce677994697
SimonDell <simonqiu4@gmail.com>
2023-09-22 04:47:17 +0800

    fix

--- UNRCE-django/UNRCE/settings.py
+++ UNRCE-django/UNRCE/settings.py
@@ -75,19 +75,31 @@ WSGI_APPLICATION = 'UNRCE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'dbmaster',
+#         'USER': 'dbmasteruser',
+#         'PASSWORD': ',Yy8N0jC<&7.,7m*kUH)TKsU_(I!j`]D',
+#         'HOST': 'ls-99842c38855ffa2f97fb191db94cd898aae44b20.c5c4ycagorbe.ap-southeast-2.rds.amazonaws.com',  # or the hostname where your MySQL server is running
+#         'PORT': '3306',      # or the port on which your MySQL server is listening
+#         'OPTIONS': {
+#             'init_command': 'SET sql_mode="STRICT_ALL_TABLES"',
+#         },
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbmaster',
-        'USER': 'dbmasteruser',
-        'PASSWORD': ',Yy8N0jC<&7.,7m*kUH)TKsU_(I!j`]D',
-        'HOST': 'ls-99842c38855ffa2f97fb191db94cd898aae44b20.c5c4ycagorbe.ap-southeast-2.rds.amazonaws.com',  # or the hostname where your MySQL server is running
-        'PORT': '3306',      # or the port on which your MySQL server is listening
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_ALL_TABLES"',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
+
+
 
 
 # Password validation
