ceda995e5c4dfc393becfec7e4011b5c37359b67
Ryan Takeda <ryantakeda1@gmail.com>
2023-09-27 14:43:02 +0800

    Made the SDGs clearer in create_project

--- UNRCE-django/UNRCE_APP/views.py
+++ UNRCE-django/UNRCE_APP/views.py
@@ -312,7 +312,13 @@ def specific_project(request):
 class CreateProject(View):
     
     def get(self, request):
-        sdgs_options = ['SDG1', 'SDG2', 'SDG3', 'SDG4', 'SDG5', 'SDG6', 'SDG7', 'SDG8', 'SDG9', 'SDG10','SDG11', 'SDG12', 'SDG13', 'SDG14', 'SDG15', 'SDG16', 'SDG17']  # List of SDGs
+        sdgs_options = ['SDG1 - No Poverty', 'SDG2 - Zero Hunger', 'SDG3 - Good Health and Well-being', 
+                        'SDG4 - Quality Education', 'SDG5 - Gender Equality', 'SDG6 - Clean Water and Sanitation', 
+                        'SDG7 - Affordable and Clean Energy', 'SDG8 - Decent Work and Economic Growth', 
+                        'SDG9 - Industry, Innovation and Infrastructure', 'SDG10 - Reduced Inequalities',
+                        'SDG11 - Sustainable Cities and Communities', 'SDG12 - Responsible Consumption and Production', 
+                        'SDG13 - Climate Action', 'SDG14 - Life Below Water', 'SDG15 - Life on Land', 
+                        'SDG16 - Peace, Justice and Strong Institutions', 'SDG17 - Partnerships for the Goals']  # List of SDGs
         
 
         delivery_frequency_options = [
