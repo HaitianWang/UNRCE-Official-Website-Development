16d6f146d652e4b27acef73cc837373281a6a664
Ryan Takeda <ryantakeda1@gmail.com>
2023-09-27 14:11:09 +0800

    Added "Save as CSV" to Admins Page

--- UNRCE-django/UNRCE_APP/admin.py
+++ UNRCE-django/UNRCE_APP/admin.py
@@ -5,6 +5,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Project
 
+# For downloading into a CSV
+import csv
+from django.http import HttpResponse
+
+
 class CustomUserAdmin(UserAdmin):
     # Define which fields are displayed on the admin page
     list_display = ('email', 'is_active', 'date_joined')
@@ -28,10 +33,29 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
-# class ProjectAdmin(admin.ModelAdmin):
-#     list_display = ("title")
-#     search_fields = ("title")
+class ProjectAdmin(admin.ModelAdmin):
+    actions = ["download_csv"]
+
+    def download_csv(self, request, queryset):
+        response = HttpResponse(content_type = "text/csv")
+        response["Content-Disposition"] = 'attachment; filename="projects.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(["ID", "Title", "Description", "Creation", "Conclusion", "Audience", "Delivery Frequency",
+                         "Language", "Web Link", "Policy Link", "Results", "Lessons Learned", "Key Messages",
+                         "Relationship to RCE Activities", "Funding", "Approval", "Status"]) # specify the header
+
+        for project in queryset:
+            writer.writerow([project.id, project.title, project.description, project.created_at, 
+                             project.concluded_on, project.audience, project.delivery_frequency,
+                             project.language, project.web_link, project.policy_link, project.results,
+                             project.lessons_learned, project.key_messages, project.relationship_to_rce_activities,
+                             project.funding, project.approval, project.status])    # specify the values
+        
+        return response
+    
+    download_csv.short_description = "Download Selected Projects as CSV"
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Image)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
