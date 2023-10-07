99b192ed1b0652165ba69294cfdc61f34f4aecbe
James Lee <jameslee6352@gmail.com>
2023-10-08 03:57:20 +0800

    Localisation

--- UNRCE-django/UNRCE/settings.py
+++ UNRCE-django/UNRCE/settings.py
@@ -122,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-au'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'AWST'
 
 USE_I18N = True
 
