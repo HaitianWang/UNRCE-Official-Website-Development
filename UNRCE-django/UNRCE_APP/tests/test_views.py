7a4733611ff620bf4c0df5c99fed3032cc2c9893
James Lee <jameslee6352@gmail.com>
2023-10-08 04:38:56 +0800

    Working login and signup form tests

--- UNRCE-django/UNRCE_APP/tests/test_views.py
+++ UNRCE-django/UNRCE_APP/tests/test_views.py
@@ -1,11 +1,57 @@
-from django.test import LiveServerTestCase, Client
+from django.test import TestCase, Client
+from UNRCE_APP.views import IndexView
+from UNRCE_APP.models import CustomUser
+from captcha.models import CaptchaStore
+from django.urls import reverse
+from django.http import HttpRequest
 
-class TestPages(LiveServerTestCase):
+class TestPages(TestCase):
+
     def setUp(self) -> None:
         self.client = Client()
         return super().setUp()
     
-    def test_landing_page(self):
-        response = self.client.get("/")
+    def get_captcha_value(self, response):
+        captcha_key = response.context.get("captcha_key")
+        captchaQuery = CaptchaStore.objects.filter(hashkey=captcha_key)
+        self.assertTrue(captchaQuery.exists())
+        self.assertEqual(captchaQuery.count(), 1)
+        captcha = captchaQuery.first()
+        return captcha.response, captcha_key
+
+    def test_signup(self):
+        response = self.client.get(reverse("UNRCE_APP:signup"))
+        self.assertEqual(response.status_code, 200)
+        captcha_value, captcha_key = self.get_captcha_value(response)
+        response = self.client.post(
+            reverse("UNRCE_APP:signup"),
+            {
+                "email":"JohnDoe@gmail.com",
+                "password1":"Is this a good password?",
+                "password2":"Is this a good password?",
+                "captcha_0":captcha_value,
+                "captcha_1":captcha_key,
+            }
+        )
+        self.assertEqual(response.status_code, 302, "Status code doesn't indicate redirection")
+        self.assertTrue(CustomUser.objects.filter(email="JohnDoe@gmail.com").exists())
         
+    #def test_signup_captcha_reuse(self):
         
+    def test_login(self):
+        user = CustomUser.objects.create(email="JohnDoe@gmail.com")
+        user.set_password("Is this a good password?")
+        user.save()
+        response = self.client.get(reverse("UNRCE_APP:login"))
+        self.assertEqual(response.status_code, 200)
+        captcha_value, captcha_key = self.get_captcha_value(response)
+        response= self.client.post(
+            reverse("UNRCE_APP:login"),
+            {
+                "username":"JohnDoe@gmail.com",
+                "password":"Is this a good password?",
+                "captcha_0":captcha_value,
+                "captcha_1":captcha_key
+            }
+        )
+        self.assertEqual(user, response.wsgi_request.user)
